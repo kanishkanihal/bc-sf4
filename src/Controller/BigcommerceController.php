d1c5d191d5c9a7de2e8b0b9168f4b5df4ec2b6c3
Kanishka Wickramarathna <kanishka.wickramarathna@netstarter.com>
2019-01-31 14:00:58 +0530

    Add basic functions

---
+++ src/Controller/BigcommerceController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Bigcommerce\Api\Connection;
+use App\Service\RedisConnector;
+
+class BigcommerceController extends AbstractController
+{
+    public $redis;
+
+    public function __construct()
+    {
+        $this->redis = new RedisConnector();
+    }
+
+    /**
+     * @Route("/bigcommerce", name="bigcommerce")
+     */
+    public function index()
+    {
+        return $this->render('bigcommerce/index.html.twig', [
+            'controller_name' => 'BigcommerceController',
+        ]);
+    }
+
+    /**
+     * @Route("/bigcommerce/callback", name="callback")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function callback(Request $request)
+    {
+        $tokenUrl = "https://login.bigcommerce.com/oauth2/token";
+        $connection = new Connection();
+        $connection->useUrlencoded();
+        $response = $connection->post($tokenUrl, array(
+            "client_id" => getenv('BC_CLIENT_ID'),
+            "client_secret" => getenv('BC_CLIENT_SECRET'),
+            "redirect_uri" => getenv('BC_CALLBACK_URL'),
+            "grant_type" => "authorization_code",
+            "code" => $request->get("code"),
+            "scope" => $request->get("scope"),
+            "context" => $request->get("context"),
+        ));
+        $token = $response->access_token;
+        $context = $response->context;
+        $this->redis->set('access_token', $token);
+        $this->redis->set('context', $context);
+
+        return $this->render('bigcommerce/index.html.twig', [
+            'controller_name' => 'BigcommerceController',
+        ]);
+    }
+
+    /**
+     * @Route("/bigcommerce/load", name="bigcommerce-load")
+     */
+    public function load()
+    {
+        return $this->render('bigcommerce/index.html.twig', [
+            'controller_name' => 'BigcommerceController',
+        ]);
+    }
+
+    /**
+     * @Route("/bigcommerce/uninstall", name="bigcommerce-uninstall")
+     */
+    public function uninstall()
+    {
+        return $this->render('bigcommerce/index.html.twig', [
+            'controller_name' => 'BigcommerceController',
+        ]);
+    }
+
+    /**
+     * @Route("/bigcommerce/remove", name="bigcommerce-remove")
+     */
+    public function remove()
+    {
+        return $this->render('bigcommerce/index.html.twig', [
+            'controller_name' => 'BigcommerceController',
+        ]);
+    }
+
+}
---
+++ src/Service/RedisConnector.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kanishka
+ * Date: 1/31/19
+ * Time: 12:12 PM
+ */
+
+namespace App\Service;
+
+use Symfony\Component\Cache\Adapter\RedisAdapter;
+use Predis\Client;
+
+class RedisConnector
+{
+    /**
+     * @var Client|\Redis|\RedisCluster
+     */
+    public $connection;
+    /**
+     * @var Client
+     */
+    public $client;
+
+    /**
+     * RedisConnector constructor.
+     */
+    public function __construct()
+    {
+        $host = getenv('REDIS_HOST');
+        $port = getenv('REDIS_PORT');;
+        $password = getenv('REDIS_PASSWORD');;
+        $db = getenv('REDIS_DATABASE');
+
+
+        $this->connection = RedisAdapter::createConnection(
+            "redis://:$password@$host:$port/$db",
+            [
+                'compression' => true,
+                'lazy' => false,
+                'persistent' => 0,
+                'persistent_id' => null,
+                'tcp_keepalive' => 0,
+                'timeout' => 30,
+                'read_timeout' => 0,
+                'retry_interval' => 0,
+            ]);
+
+        $this->client = new Client();
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     */
+    public function set($key, $value)
+    {
+        $this->connection->set($key, $value);
+    }
+
+    /**
+     * @param $key
+     * @return bool|string
+     */
+    public function get($key)
+    {
+        return $this->connection->get($key);
+    }
+
+
+}
